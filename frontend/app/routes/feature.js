b6bc3369b7f01889151479cfd588012023bced13
mcoqblin <mathias.coqblin@gmail.com>
2018-06-27 16:24:06 +0200

    Added basic error check on a wrong ID provided to /feature (will redirect to /features)

--- frontend/app/routes/feature.js
+++ frontend/app/routes/feature.js
@@ -3,5 +3,17 @@ import Route from '@ember/routing/route';
 export default Route.extend({
     model(params) {
         return this.get('store').findRecord('feature', params.feature_id, { include: 'tests'});
+    },
+
+    actions: {
+        error(error) {
+            if ((error.errors[0].status === '404')
+                || (error.errors[0].status === '500')) {
+                this.replaceWith('features');
+            }
+            else {
+                return true;
+            }
+        }
     }
 });
