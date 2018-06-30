82bc403359927a40054210f4a6e1108da292bd15
mcoqblin <mathias.coqblin@gmail.com>
2018-06-30 11:02:07 +0200

    Some extra error code for the feature route

--- frontend/app/routes/feature.js
+++ frontend/app/routes/feature.js
@@ -7,6 +7,9 @@ export default Route.extend({
 
     actions: {
         error(error) {
+            if (!error.errors) {
+                this.replaceWith('features');
+            }
             if ((error.errors[0].status === '404')
                 || (error.errors[0].status === '500')) {
                 this.replaceWith('features');
